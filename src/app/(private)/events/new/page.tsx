ef98ca6b0ae43c21985c0ae73a21da9e0278f38b
David R <elidovrich.coding@gmail.com>
2024-10-23 17:59:43 -0400

    add backend functy to eventsPage

---
+++ src/app/(private)/events/new/page.tsx
@@ -0,0 +1,9 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function NewEventPage() {
+    return (
+        <Card>
+            <CardHeader><CardTitle>New Event</CardTitle></CardHeader>
+        </Card>
+    ) 
+}
--- src/app/(private)/events/page.tsx
+++ src/app/(private)/events/page.tsx
@@ -1,14 +1,45 @@
 import { Button } from "@/components/ui/button";
-import { CalendarPlus } from "lucide-react";
+import { db } from "@/drizzle/db";
+import { auth } from "@clerk/nextjs/server";
+import { CalendarPlus, CalendarRange } from "lucide-react";
 import Link from "next/link";
 
-export default function page() {
-    return (
-        <>
-            <div className="flex gap-4 items-baseline">
-                <h1 className="text-3xl lg:text-4xl xl:text-5xl font-semibold mb-6">Events</h1>
-                <Button asChild><Link href="/events/new"><CalendarPlus className="mr-4 size-6" />New Event</Link></Button>
-            </div>
-        </>
-    )
+export default async function EventsPage() {
+  const { userId, redirectToSignIn } = auth();
+  if (userId === null) {
+    return redirectToSignIn();
+  }
+  const events = await db.query.EventTable.findMany({
+    where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
+    orderBy: ({ createdAt }, { desc }) => desc(createdAt),
+  });
+  return (
+    <>
+      <div className="flex gap-4 items-baseline">
+        <h1 className="text-3xl lg:text-4xl xl:text-5xl font-semibold mb-6">
+          Events
+        </h1>
+        <Button asChild>
+          <Link href="/events/new">
+            <CalendarPlus className="mr-4 size-6" />
+            New Event
+          </Link>
+        </Button>
+      </div>
+      {events.length > 0 ? (
+        <h1>Events</h1>
+      ) : (
+        <div className="flex flex-col items-center gap-4">
+          <CalendarRange className="size-16 mx-auto" />
+          You do not have any events yet. Create your first event to get
+          started!
+          <Button size="lg" className="text-lg" asChild>
+            <Link href="/events/new">
+              <CalendarPlus className="mr-4 size-6" /> New Event
+            </Link>
+          </Button>
+        </div>
+      )}
+    </>
+  );
 }
