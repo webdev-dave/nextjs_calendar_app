af2a73bb098f9620c3e3d93dd8e1d8723df910a8
David R <elidovrich.coding@gmail.com>
2024-10-23 18:01:31 -0400

    add new event page

--- src/app/(private)/events/new/page.tsx
+++ src/app/(private)/events/new/page.tsx
@@ -1,9 +1,12 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function NewEventPage() {
     return (
         <Card>
-            <CardHeader><CardTitle>New Event</CardTitle></CardHeader>
+            <CardHeader>
+                <CardTitle>New Event</CardTitle>
+            </CardHeader>
+            <CardContent>content</CardContent>
         </Card>
-    ) 
+    );
 }
